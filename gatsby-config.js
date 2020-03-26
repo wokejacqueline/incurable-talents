c040b923eff1baf3b6f76bb810b248ae69932135
Jacqueline Matuszak <doggos@Jacquelines-MacBook-Pro.local>
2020-03-26 13:40:35 -0700

    make analytics work lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,11 +34,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-102759504-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
+        head: true,
         // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
